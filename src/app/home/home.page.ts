eba91713d71da90f352e957e0067fb1de7b23a73
PedroHAMota <henrypedrinho11@gmail.com>
2023-09-12 18:17:47 -0300

    Funcionarios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,62 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+  isModalOpen = false;
+  setOpen(isOpen: boolean){
+    this.isModalOpen = isOpen;
+  }
+  isLoading: boolean = false;
+  funcionarios: any;
+
+  constructor() {
+    this.getFuncionarios()
+  }
+
+
+
+  getFuncionarios(){
+    this.isLoading = true;
+    fetch('http://localhost/api2/funcionarios/listar.php')
+    .then(response => response.json())
+    .then(response => {
+      this.funcionarios = response['funcionarios']
+      console.log(response)
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.isLoading = false;
+    })
+  }
+
+  remover(codigo:any){
+    this.isLoading = true;
+    fetch('http://localhost/api2/funcionarios/remover.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify({ CodFun: codigo })
+			}
+		)
+    .then(response => response.json())
+    .then(response => {
+      this.getFuncionarios();
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.isLoading = false;
+    })
+  }
+
+
 }
+
